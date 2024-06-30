92533688fb7652ad46185237b0d16be29a2e6605
石塚直樹 <d.r-1004@i.softbank.jp>
2024-06-30 22:28:55 +0900

    feat: threegcd

---
+++ csMiddle/recursion/threegcd/Main.java
@@ -0,0 +1,17 @@
+package csMiddle.recursion.threegdc;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(threeGCD(12, 18, 24));
+    }
+
+    public static int threeGCD(int x, int y, int z){
+        int a = gcd(x, y);
+        return gcd(z, a);
+    }
+
+    public static int gcd(int a, int b) {
+        if(a%b == 0) return b;
+        return gcd(b, a%b);
+    }
+}
